08199bd9e04c2349fb610aefb2fbf38e2142d272
tanishaggarwal <tanishaggarwal9357@gmail.com>
2020-08-10 13:11:15 +0530

    alarms on received google meet's request

--- app/src/main/java/com/example/classalert/MainActivity.java
+++ app/src/main/java/com/example/classalert/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-        startActivity(intent);
+        btn=findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+                startActivity(intent);
+            }
+        });
+
 
 
 
--- app/src/main/java/com/example/classalert/MainActivity2.java
+++ app/src/main/java/com/example/classalert/MainActivity2.java
@@ -2,9 +2,14 @@ package com.example.classalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -19,13 +24,32 @@ public class MainActivity2 extends AppCompatActivity {
     Button btn;
     ImageView ivAlarm;
     TextView tvAlert;
-
+    PowerManager.WakeLock wakeLock;
     Animation animBlink;
+    KeyguardManager km;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+        km = (KeyguardManager) this.getSystemService(Context.KEYGUARD_SERVICE);
+        final KeyguardManager.KeyguardLock kl = km.newKeyguardLock("MyKeyguardLock");
+        kl.disableKeyguard();
+
+
+        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                | PowerManager.ON_AFTER_RELEASE, "MyWakeLock");
+        wakeLock.acquire();
 
         ivAlarm = findViewById(R.id.ivAlarm);
         tvAlert = findViewById(R.id.tvAlert);
@@ -36,18 +60,33 @@ public class MainActivity2 extends AppCompatActivity {
         tvAlert.startAnimation(animBlink);
 
         mp= MediaPlayer.create(this,R.raw.fantasy_alarm_clock);
-        mp.setLooping(true);
+//        mp.setLooping(true);
         mp.start();
         btn=findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mp!=null) {
-                    mp.stop();
-                    mp.release();
+                if(mp.isPlaying()) {
                     finish();
                 }
             }
         });
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        wakeLock.release();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mp!=null && mp.isPlaying()) {
+            mp.stop();
+            finish();
+        }
+    }
+
+
 }
